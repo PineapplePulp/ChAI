50fa0d93e2ffc8a8073b2936e850b5f44476f03a
Iain Moncrief <moncrief@oregonstate.edu>
2025-03-11 22:10:55 -0700

    Move to HPC system.

--- examples/cat_breeds/cat_breeds.py
+++ examples/cat_breeds/cat_breeds.py
@@ -170,10 +170,12 @@ class SmallCNN(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Flatten(),
-            nn.Dropout(0.25),
-            nn.LazyLinear(1024),
+            nn.Dropout(0.5),
+            nn.LazyLinear(2048),
             nn.ReLU(),
-            nn.Linear(1024, 12),
+            nn.LazyLinear(512),
+            nn.ReLU(),
+            nn.LazyLinear(12),
             nn.LogSoftmax(dim=1)
         )
 
@@ -211,6 +213,7 @@ def train(model,
 
     if verbose:
         print(f'Train Epoch: {epoch} \tAverage loss: {avg_loss:.6f}')
+    return avg_loss
 
 def eval(model, 
           device, 
@@ -232,7 +235,8 @@ def eval(model,
     
     avg_loss /= len(test_loader.dataset)
 
-    print(f'Test Epoch: {epoch} \tAverage loss: {avg_loss:.6f}')
+    # print(f'Test Epoch: {epoch} \tAverage loss: {avg_loss:.6f}')
+    return avg_loss
 
 
 # def eval(model, 
@@ -258,15 +262,32 @@ def eval(model,
 #     print(f'\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} ({100. * correct / len(test_loader.dataset):.0f}%)\n')
 
 
+def get_attributes(model):
+    return { str(k): str(v[0]) if isinstance(v,tuple) else str(v).lower() for (k,v) in dict(model.__dict__).items() if k[0] != '_'}
+
+def get_summary(model,global_name,parent_name=None):
+    model_name = model.__class__.__name__
+    d = {
+        'layerType': model_name,
+        'attributes': get_attributes(model),
+        'subModules': { name : get_summary(m,global_name=global_name,parent_name=name) for name,m in  model.named_children() },
+        'subModuleOrder': [name for name,_ in model.named_children()]
+    }
+    return d
+
+def has_same_architecture(model1, model2):
+    return get_summary(model1,'') == get_summary(model2,'')
+
 if __name__ == "__main__":
     my_dir = Path(__file__).resolve().parent
     data_dir = my_dir / 'data'
+    model_path = my_dir / 'models' / 'pretest.pt'
 
     print('Constructing CatBreedsData Dataset.')
     data_set = CatBreedsData(data_dir)
     
     data_set_size = len(data_set)
-    train_len = int(data_set_size*0.7)      
+    train_len = int(data_set_size*0.9)      
     test_len = data_set_size - train_len
     train_set, test_set = torch.utils.data.random_split(data_set, [train_len, test_len])
 
@@ -274,14 +295,15 @@ if __name__ == "__main__":
     print('Test set size:', len(test_set))
 
     print('Creating DataLoader(s).')
-    train_batch_size = 20
+    train_batch_size = test_len
     train_loader = DataLoader(train_set, batch_size=train_batch_size, shuffle=True)
 
     test_batch_size = test_len
-    test_loader = DataLoader(test_set, batch_size=test_batch_size, shuffle=False)
+    test_loader = DataLoader(test_set, batch_size=test_batch_size, shuffle=True)
 
-    # train_features, train_labels = next(iter(train_loader))
-    # print(train_features[0].shape,train_labels[0])
+    test_features, test_labels = next(iter(test_loader))
+    print(test_features[0].shape,test_labels[0])
+    print(test_features[1].shape,test_labels[1])
 
     device = get_best_device()
     print('Using device:', device)
@@ -289,10 +311,19 @@ if __name__ == "__main__":
     print('Creating model...')
     model = SmallCNN()
 
+    # if model_path.exists():
+    #     print('Loading model from', model_path)
+    #     model1 = torch.load(model_path)
+    #     if has_same_architecture(model, model1):
+    #         print('Model architecture match. Using existing model.')
+    #         model = model1
+    #     else:
+    #         print('Model architecture mismatch. Using new model.')
+
     print('Moving model to device...')
     model.to(device)
 
-    epochs = 100
+    epochs = 400
 
     print('Constructing optimizer and criterion...')
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
@@ -300,11 +331,12 @@ if __name__ == "__main__":
 
     print('Starting training...')
     for epoch in range(epochs):
-        train(model, device, train_loader, optimizer, criterion, epoch, one_pass=False, verbose=True)
-        # eval(model, device, test_loader, optimizer, criterion, epoch)
+        train_loss = train(model, device, train_loader, optimizer, criterion, epoch, one_pass=False, verbose=False)
+        eval_loss = eval(model, device, test_loader, optimizer, criterion, epoch)
+        print(f'Epoch {epoch} \tTrain loss: {train_loss:.6f} \tTest loss: {eval_loss:.6f}')
         
     model.to(torch.device('cpu'))
-    torch.save(model, my_dir / 'models' / 'pretest.pt')
+    torch.save(model, model_path)
     print("Model saved to", my_dir / 'models' / 'pretest.pt')
 
 
